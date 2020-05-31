ef497b30c3805fa82cd17451de075953f45a94eb
Saphie <safihanadi28@gmail.com>
2020-05-31 17:36:29 +0300

    *feat

--- config/express.js
+++ config/express.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 module.exports = (app) => {
-    
-    //TODO: Setup the view engine
-
-    //TODO: Setup the body parser
-
-    //TODO: Setup the static files
-
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.engine(".hbs", handlebars({ extname: ".hbs" }));
+  app.set("view engine", ".hbs");
+  app.use("/static", express.static("static"));
 };
