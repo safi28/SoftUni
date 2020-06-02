9aefda07608e6aaf6b8f7b289cff36578154227d
Saphie <safihanadi28@gmail.com>
2020-06-02 17:38:02 +0300

    updated

--- JS-WEB/JS-Back-End/Express-Templating-Workshop/config/express.js
+++ JS-WEB/JS-Back-End/Express-Templating-Workshop/config/express.js
@@ -1,15 +1,6 @@
-
-const express = require("./node_modules/express");
-const handlebars = require("express-handlebars");
-const bodyParser = require("./node_modules/body-parser");
-
-const handlebars = require("express-handlebars");
-const express = require("express");
-
 const express = require("express");
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
-
 
 module.exports = (app) => {
   app.use(express.json());
@@ -23,7 +14,6 @@ module.exports = (app) => {
   );
 
   app.set("view engine", ".hbs");
-
 
   app.engine(".hbs", handlebars({ extname: ".hbs" }));
   app.set("view engine", ".hbs");
