282ddb545b3355ac3dd6aca622cbba0eb82f8bb8
Saphie <safihanadi28@gmail.com>
2020-06-02 16:59:38 +0300

    configuration for express.js and handlebars

--- JS-WEB/JS-Back-End/Express-Templating-Workshop/config/express.js
+++ JS-WEB/JS-Back-End/Express-Templating-Workshop/config/express.js
@@ -1,12 +1,24 @@
+<<<<<<< Updated upstream
 
 const express = require("./node_modules/express");
 const handlebars = require("express-handlebars");
 const bodyParser = require("./node_modules/body-parser");
+=======
+const handlebars = require("express-handlebars");
+const express = require("express");
+>>>>>>> Stashed changes
 
 module.exports = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.engine(".hbs", handlebars({ extname: ".hbs" }));
+  app.engine(
+    ".hbs",
+    handlebars({
+      extname: ".hbs",
+    })
+  );
+
   app.set("view engine", ".hbs");
+
   app.use("/static", express.static("static"));
 };
