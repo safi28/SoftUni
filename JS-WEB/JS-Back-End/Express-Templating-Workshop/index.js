da0da81d304ef1e26f38327f9fe98f8a29734571
Saphie <safihanadi28@gmail.com>
2020-06-02 17:02:11 +0300

    app use file

--- JS-WEB/JS-Back-End/Express-Templating-Workshop/index.js
+++ JS-WEB/JS-Back-End/Express-Templating-Workshop/index.js
@@ -1,9 +1,20 @@
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 
+<<<<<<< Updated upstream
 const config = require('./config/config')[env];
 const app = require('./node_modules/express')();
+=======
+const config = require("./config/config")[env];
+const express = require("express");
+const indexRouter = require("./routes");
+const app = express();
+>>>>>>> Stashed changes
 
-require('./config/express')(app);
-require('./config/routes')(app);
+require("./config/express")(app);
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.use("/", indexRouter);
+
+app.listen(
+  config.port,
+  console.log(`Listening on port ${config.port}! Now its up to you...`)
+);
