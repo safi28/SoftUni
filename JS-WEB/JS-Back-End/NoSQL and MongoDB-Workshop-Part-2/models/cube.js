5f96d7224e9c276cee835a15821c77bbfa991758
Saphie <safihanadi28@gmail.com>
2020-06-04 21:40:17 +0300

    mongoose schema models

---
+++ JS-WEB/JS-Back-End/NoSQL and MongoDB-Workshop-Part-2/models/cube.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const cubeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  imageUrl: String,
+  difficulty: Number,
+  accessories: [{ type: mongoose.Types.ObjectId, ref: "Accessories" }],
+});
+module.exports = mongoose.model("Cube", cubeSchema);
