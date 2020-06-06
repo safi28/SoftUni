90ab5632c23365a06d7fa5db356e93c2fce6f17f
Saphie <safihanadi28@gmail.com>
2020-06-06 23:30:54 +0300

    express routing

--- JS-WEB/JS-Back-End/NoSQL-MongoDB-Workshop-Part-2/routes/index.js
+++ JS-WEB/JS-Back-End/NoSQL-MongoDB-Workshop-Part-2/routes/index.js
@@ -1,12 +1,19 @@
 const { Router } = require("express");
-const { cubeController, accessoryController } = require("../controllers");
-const { cubeModel, accessoryModel } = require("../models");
+const {
+  getAccessories,
+  postAccessories,
+} = require("../controllers/accessories");
+const { getCube, getCubes, deleteCube } = require("../controllers/cubes");
+const { cubeModel } = require("../models");
+const Accessory = require("../models/accessory");
 const router = Router();
 
-router.get("/", cubeController.index);
-
-router.get("/attach/accessory/:id", accessoryController.attachGet);
-router.post("/attach/accessory/:id", accessoryController.attachPost);
+router.get("/", async (req, res) => {
+  const cubes = await getCubes();
+  res.render("index", {
+    cubes
+  });
+});
 
 router.get("/create", (req, res) => {
   res.render("create", {
@@ -22,30 +29,61 @@ router.post("/create", (req, res) => {
   });
 });
 
-router.get("/details/:id", cubeController.details);
-
-router.get("/create/accessory", (req, res) => {
-  res.render("createAccessory", {
-    title: "Create Accessory | Cube Workshop",
+router.get("/details/:id", async (req, res) => {
+  const cube = await getCube(req.params.id);
+  res.render("details", {
+    ...cube,
   });
 });
-router.post("/create/accessory", (req, res) => {
-  const { name, description, imageUrl, difficulty } = req.body;
-  accessoryModel
-    .create({ name, description, imageUrl, difficulty })
-    .then((created) => {
-      console.log(created);
-      res.redirect("/");
-    })
-    .catch(console.error);
-});
 
+router.get("/delete", async (req, res) => {
+  await deleteCube(req.params.id);
+  res.redirect("/");
+});
 router.get("/about", (req, res) => {
   res.render("about", {
     title: "About | Cube Workshop",
   });
 });
-router.get("/delete", cubeController.deleteCube);
+router.get("/create/accessory", (req, res) => {
+  res.render("createAccessory", {
+    title: "Create Accessory | Cube Workshop",
+  });
+});
+router.post("/create/accessory", async (req, res) => {
+  const { name, description, imageUrl, difficulty } = req.body;
+  const accessory = new Accessory({ name, description, imageUrl, difficulty });
+  await accessory.save();
+  res.redirect("/");
+});
+
+router.get("/attach/accessory/:id", async (req, res) => {
+  const cube = await getCube(req.params.id);
+  const accessories = await getAccessories();
+
+  const cubeAccessories = cube.accessories.map((acc) =>
+    acc._id.valueOf().toString()
+  );
+
+  const notAttachedAccessories = accessories.filter((acc) => {
+    const accString = acc._id.valueOf().toString();
+    return !cubeAccessories.includes(accString);
+  });
+
+  const attachAccessory =
+    cube.accessories.length !== accessories.length && accessories.length > 0;
+
+  res.render("attachAccessory", {
+    ...cube,
+    acccessories: notAttachedAccessories,
+    attachAccessory,
+  });
+});
+router.post("/attach/accessory/:id", async (req, res) => {
+  const { accessory } = req.body;
+  await postAccessories(req.params.id, accessory);
+  res.redirect(`/details/${req.params.id}`);
+});
 router.get("*", (req, res) => {
   res.render("404", {
     title: "Error | Cube Workshop",
